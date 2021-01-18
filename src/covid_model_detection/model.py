49c0babc266d1e7e00d18ae4dbb746f8fd3fd367
Ryan Barber <rmbarber@uw.edu>
2021-01-18 15:46:58 -0800

    apply random effect from parent location to children; store avg testing date

--- src/covid_model_detection/model.py
+++ src/covid_model_detection/model.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple, Dict
+from loguru import logger
 import pandas as pd
 import numpy as np
 
@@ -46,6 +47,7 @@ def idr_model(model_data: pd.DataFrame,
 
 
 def predict(all_data: pd.DataFrame,
+            hierarchy: pd.DataFrame,
             fixed_effects: pd.Series, random_effects: pd.Series,
             pred_replace_dict: Dict,
             dep_var: str,
@@ -55,6 +57,7 @@ def predict(all_data: pd.DataFrame,
         raise ValueError('Duplicate in replace_var + indep_vars.')
     pred_data = (all_data
                  .loc[:, ['location_id', 'date'] + keep_vars]
+                 .dropna()
                  .drop_duplicates()
                  .set_index(['location_id', 'date'])
                  .sort_index())
@@ -64,18 +67,42 @@ def predict(all_data: pd.DataFrame,
                     .multiply(fixed_effects)
                     .sum(axis=1)
                     .rename(f'{dep_var}_fe'))
+    
+    # duplicate REs for child locations
+    parent_ids = [location_id for location_id in random_effects.index if location_id in hierarchy.loc[hierarchy['most_detailed'] == 0, 'location_id'].to_list()]
+    child_ids_lists = [hierarchy.loc[hierarchy['path_to_top_parent'].str.contains(f',{parent_id},'), 'location_id'].to_list() for parent_id in parent_ids]
+    child_ids_lists = [list(set(child_ids) - set(random_effects.index)) for child_ids in child_ids_lists]
+    parent_children_pairs = list(zip(parent_ids, child_ids_lists))
+    parent_children_pairs = [(parent_id, child_ids) for parent_id, child_ids in parent_children_pairs if len(child_ids) > 0]
+    
+    parent_random_effects = []
+    for parent_id, child_ids in parent_children_pairs:
+        parent_name = hierarchy.loc[hierarchy['location_id'] == parent_id, 'location_name'].item()
+        child_names = hierarchy.loc[hierarchy['location_id'].isin(child_ids), 'location_name'].to_list()
+        child_names = ', '.join(child_names)
+        logger.info(f'Using parent {parent_name} RE for {child_names}.')
+        parent_random_effects.append(pd.Series(random_effects[parent_id], index=pd.Index(child_ids, name='location_id')))
+    pd.concat([random_effects] + parent_random_effects)
+    random_effects = pd.concat([random_effects] + parent_random_effects).sort_index()
+    if not random_effects.index.is_unique:
+        raise ValueError('Duplicated random effect in process of applying parents.')
+    
     pred_data_w_re = ((pred_data_fe + random_effects)
                        .dropna()
                        .rename(f'{dep_var}_w_re'))
     pred_data = pd.concat([pred_data, pred_data_fe, pred_data_w_re], axis=1)
+    pred_data_fe = pred_data[f'{dep_var}_fe']
     pred_data = (pred_data[f'{dep_var}_w_re']
                  .fillna(pred_data[f'{dep_var}_fe'])
                  .rename('idr'))
+    pred_data_fe = pred_data_fe.rename('idr_fe')
     if dep_var == 'logit_idr':
         pred_data = expit(pred_data)
+        pred_data_fe = expit(pred_data_fe)
     elif dep_var == 'log_idr':
         pred_data = np.exp(pred_data)
+        pred_data_fe = np.exp(pred_data_fe)
     elif dep_var != 'idr':
         raise ValueError('Unexpected transformation of IDR in model; cannot predict.')
     
-    return pred_data
+    return pred_data, pred_data_fe
--- src/covid_model_detection/runner.py
+++ src/covid_model_detection/runner.py
@@ -19,14 +19,15 @@ def main(app_metadata: cli_tools.Metadata,
          output_root: Path, n_draws: int):
     hierarchy = data.load_hierarchy(model_inputs_root)
     pop_data = data.load_population(model_inputs_root)
-    full_sero_data, sero_data = data.load_serosurveys(model_inputs_root)
-    case_data = data.load_cases(model_inputs_root)
-    test_data = data.load_testing(testing_root)
+    sero_data = data.load_serosurveys(model_inputs_root)
+    case_data = data.load_cases(model_inputs_root, hierarchy)
+    test_data = data.load_testing(testing_root, hierarchy)
     
     var_args = {'dep_var': 'logit_idr',
                 'dep_var_se': 'logit_idr_se',
                 'indep_vars': ['intercept', 'log_avg_daily_testing_rate']}
     pred_replace_dict = {'log_daily_testing_rate': 'log_avg_daily_testing_rate'}
+    model_space_suffix = 'avg_testing'
     
     all_data, model_data = data.prepare_model_data(
         hierarchy=hierarchy,
@@ -40,13 +41,18 @@ def main(app_metadata: cli_tools.Metadata,
     )
     
     mr_model, fixed_effects, random_effects = model.idr_model(model_data=model_data, **var_args)
-    pred_idr = model.predict(all_data, fixed_effects, random_effects, pred_replace_dict, **var_args)
-    
-    pred_idr_model_space = model.predict(all_data, fixed_effects, random_effects, {}, **var_args)
-    all_data = all_data.merge(pred_idr_model_space.rename('pred_idr_model_space').reset_index())
+    pred_idr, pred_idr_fe = model.predict(all_data, hierarchy, fixed_effects, random_effects, pred_replace_dict, **var_args)
+
+    pred_idr_all_data, _ = model.predict(all_data, hierarchy, fixed_effects, random_effects,
+                                         pred_replace_dict, **var_args)
+    pred_idr_all_data_model_space, _ = model.predict(all_data, hierarchy, fixed_effects, random_effects,
+                                                     {}, **var_args)
+    all_data = all_data.merge(pred_idr_all_data.rename('pred_idr').reset_index())
+    all_data = all_data.merge(pred_idr_all_data_model_space.rename(f'pred_idr_{model_space_suffix}').reset_index())
+    all_data['in_model'] = all_data['data_id'].isin(model_data['data_id'].to_list()).astype(int)
     
     sero_path = output_root / 'sero_data.csv'
-    full_sero_data.to_csv(sero_path, index=False)
+    sero_data.to_csv(sero_path, index=False)
     
     test_path = output_root / 'test_data.csv'
     test_data.to_csv(test_path, index=False)
@@ -59,4 +65,5 @@ def main(app_metadata: cli_tools.Metadata,
         pickle.dump((mr_model, fixed_effects, random_effects), file, -1)
     
     pred_path = output_root / 'pred_idr.csv'
+    pred_idr = pd.concat([pred_idr, pred_idr_fe], axis=1)
     pred_idr.reset_index().to_csv(pred_path, index=False)
